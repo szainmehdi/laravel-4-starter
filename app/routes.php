d2d4538f8a86c87d52ae62bbaf5fff0f692afbf3
S. Zain Mehdi <szainmehdi@gmail.com>
2014-11-14 00:18:08 -0800

    Removed routes from main routes file. Keep it there just for now.

--- app/routes.php
+++ app/routes.php
@@ -2,22 +2,10 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Load The Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
+| Our application routes are stores in a PSR-4 compliant namespace.
+| Look in \App\Routing\RouteServiceProvider to get started.
 |
 */
-
-Route::get('/', function()
-{
-	return View::make('hello');
-});
-
-// Confide RESTful route
-//Route::get('users/confirm/{code}', 'UsersController@getConfirm');
-//Route::get('users/reset_password/{token}', 'UsersController@getReset');
-//Route::get('users/reset_password', 'UsersController@postReset');
-//Route::controller('users', 'UsersController');
